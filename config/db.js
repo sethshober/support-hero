a762de5148aebe38eff082f3e67d09d212072c03
Seth Shober <seth.shober@gmail.com>
2016-02-18 22:14:23 -0800

    scaffold and db connection

---
+++ config/db.js
@@ -0,0 +1,13 @@
+var mongoose = require('mongoose')
+var db = mongoose.connection
+var uri = 'mongodb://' + process.env.DB_USER + ':' + process.env.DB_PASS + '@ds011278.mongolab.com:11278/support-hero'
+
+mongoose.connect(uri , function(){
+  db.on('error', console.error.bind(console, 'connection error:')) 
+ 
+    db.once('open', function(){
+        console.log("Successfully connected to support-hero MongoDB!")
+    })
+})
+
+module.exports = mongoose
