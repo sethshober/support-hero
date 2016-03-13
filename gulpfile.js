1c0518e9348f269fb6433999a43b8e527de4d87d
Seth Shober <seth.shober@gmail.com>
2016-03-12 22:03:22 -0800

    add basic linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,29 @@
 var gulp    = require('gulp')
+  , jshint  = require('gulp-jshint')
   , concat  = require('gulp-concat')
   , nodemon = require('gulp-nodemon')
   , babel   = require("gulp-babel")
+
+//var packageJSON  = require('./package');
+//var jshintConfig = packageJSON.jshintConfig;
+
+//jshintConfig.lookup = false;
 
 var config = {
   jsPath: 'public/js/ng/**/*.js',
 }
 
-gulp.task('js', function(){
+gulp.task('js', ['lint'], function(){
   gulp.src(['public/js/ng/module.js', config.jsPath])
     .pipe(babel())
     .pipe(concat('js/app.js'))
     .pipe(gulp.dest('public'))
+})
+
+gulp.task('lint', function() {
+  return gulp.src(config.jsPath)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
 })
 
 gulp.task('watch:js', ['js'], function(){
